9e549c64953b7fdf9e1e7aa47c39dfca3ef68620
Arcane <50653080+arcane-cs@users.noreply.github.com>
2026-01-21 04:58:26 +0800

    Added Echo function

--- src/main/java/Gigi.java
+++ src/main/java/Gigi.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Gigi {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = "  ________.__       .__ \n" +
                 " /  _____/|__| ____ |__|\n" +
                 "/   \\  ___|  |/ ___\\|  |\n" +
@@ -8,6 +12,15 @@ public class Gigi {
                 "        \\/  /_____/     ";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to you see you again soon!");
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to you see you again soon!");
+                scanner.close();
+                return;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
