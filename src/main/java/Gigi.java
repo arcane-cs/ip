c9b571b4e17591f062fc6bd4f59a9ed81be1909f
Arcane <50653080+arcane-cs@users.noreply.github.com>
2026-01-23 04:18:30 +0800

    Added Error handling for menu logic

--- src/main/java/Gigi.java
+++ src/main/java/Gigi.java
@@ -28,39 +28,58 @@ public class Gigi {
                     System.out.println("\t"+ (i+1) + ". " + list.get(i));
                 }
             } else {
-                String[] split = input.split(" ", 2);
-                String command = split[0];
-                if (split.length < 2) {
-                    split = new String[]{split[0], ""};
-                }
-                String arguments = split[1];
-                if (command.equals("mark")) {
-                    list.get(Integer.parseInt(arguments)-1).markDone();
-                    System.out.println("I have marked the task as done:");
-                    System.out.println("\t"+list.get(Integer.parseInt(arguments)-1));
-                } else if (command.equals("unmark")) {
-                    list.get(Integer.parseInt(arguments)-1).unmarkDone();
-                    System.out.println("I have unmarked the task as done:");
-                    System.out.println("\t"+list.get(Integer.parseInt(arguments)-1));
-                } else {
-                    Task newTask;
-                    if (command.equals("deadline")) {
-                        String[] a = arguments.split("/by", 2);
-                        newTask = new Deadline(a[0], a[1]);
-                    } else if (command.equals("event")) {
-                        String[] a = arguments.split("/from", 2);
-                        String description = a[0];
-                        a = a[1].split("/to", 2);
-                        newTask = new Event(description, a[0], a[1]);
-                    } else if (command.equals("todo")){
-                        newTask = new Todo(arguments);
-                    } else {
-                        System.out.println("Invalid command :(");
-                        continue;
+                try {
+                    String[] split = input.split(" ", 2);
+                    String command = split[0];
+                    if (split.length < 2) {
+                        throw new GigiException("Please give a valid command!");
                     }
-                    list.add(newTask);
-                    System.out.println("added: \n\t" + newTask);
-                    System.out.println("There are now " + list.size() + " tasks in the list.");
+                    String arguments = split[1];
+                    if (command.equals("mark")) {
+                        list.get(Integer.parseInt(arguments)-1).markDone();
+                        System.out.println("I have marked the task as done:");
+                        System.out.println("\t"+list.get(Integer.parseInt(arguments)-1));
+                    } else if (command.equals("unmark")) {
+                        list.get(Integer.parseInt(arguments)-1).unmarkDone();
+                        System.out.println("I have unmarked the task as done:");
+                        System.out.println("\t"+list.get(Integer.parseInt(arguments)-1));
+                    } else {
+                        Task newTask;
+                        if (command.equals("deadline")) {
+                            String[] a = arguments.split("/by", 2);
+                            if (a.length < 2) {
+                                throw new GigiException("Provide a 'by' for your deadline!");
+                            }
+                            newTask = new Deadline(a[0], a[1]);
+                        } else if (command.equals("event")) {
+                            String[] a = arguments.split("/from", 2);
+                            String description = a[0];
+                            if (a.length < 2) {
+                                throw new GigiException("Provide a 'from' for your event!");
+                            }
+                            a = a[1].split("/to", 2);
+                            if (a.length < 2) {
+                                throw new GigiException("Provide a 'to' for your event!");
+                            }
+                            newTask = new Event(description, a[0], a[1]);
+                        } else if (command.equals("todo")) {
+                            newTask = new Todo(arguments);
+                        } else {
+                            System.out.println("Invalid command :(");
+                            continue;
+                        }
+                        list.add(newTask);
+                        System.out.println("added: \n\t" + newTask);
+                        System.out.println("There are now " + list.size() + " tasks in the list.");
+
+                    }
+                } catch (GigiException e) {
+                    System.out.println(e.getMessage());
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println("Give a valid index!");
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println("\t"+ (i+1) + ". " + list.get(i));
+                    }
                 }
             }
         }
---
+++ src/main/java/GigiException.java
@@ -0,0 +1,5 @@
+public class GigiException extends RuntimeException {
+    public GigiException(String message) {
+        super(message);
+    }
+}
